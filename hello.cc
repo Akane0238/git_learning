9d524d673ed47a99135cdbe837e281da3bf4cc0a
akane <qweasd28848@gmail.com>
2024-10-07 14:42:49 +0800

    In hello.cc: define a head node

--- hello.cc
+++ hello.cc
@@ -7,5 +7,10 @@ struct node{
 
 int main(){
     cout << "Hello world!" << endl;
+    cout << "I gona build a linked list" << endl;
+    
+    node* head = new node;
+    head->val = 238;
+    head->next = nullptr;
     return 0;
 }	
